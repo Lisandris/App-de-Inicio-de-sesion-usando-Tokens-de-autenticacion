f19f6897317ae5fbf47ee17765644786e4ff9c02
Lisandris <lis170694@gmail.com>
2022-09-13 23:19:25 +0200

    Formularios de Login y reutilizacion de inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_apps/ui/input_decorations.dart';
 import 'package:productos_apps/widgets/widgets.dart';
 
 
@@ -23,15 +24,78 @@ class LoginScreen extends StatelessWidget {
                     Text('Login', style: Theme.of(context).textTheme.headline5),
                     SizedBox( height: 30 ),
 
-                    Text('Foramulario')
+                    _LogibForm(),
                   ],
                 ),
               ),
 
+              SizedBox( height: 50 ),
+              Text('Crear nueva cuenta', style:TextStyle( fontSize:20, fontWeight: FontWeight.bold ), ),
+              SizedBox( height: 50 ),
             ],
           ),
         )
       )
+    );
+  }
+}
+
+class _LogibForm extends StatelessWidget {
+  const _LogibForm
+  ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        // TODO: mantener la ref al KEY
+        child: Column(
+          children:  [
+
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'example@gmail.com',
+                labelText: 'Correo electronico',
+                prefixIcon: Icons.alternate_email_rounded,
+              ),
+            ),
+
+             const SizedBox( height: 30 ),
+
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '******',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock_outline_rounded,
+              ),
+            ),
+
+             SizedBox( height: 30 ),
+
+            MaterialButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Color.fromARGB(255, 201, 90, 182),
+              child: Container(
+                padding: EdgeInsets.symmetric( horizontal: 70, vertical: 15),
+                child: Text('Ingresar',
+                 style: TextStyle ( color: Colors.white ),
+                 ),
+              ),
+              onPressed: (){
+                // TODO: Login form 
+              }
+              )
+            
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/ui/input_decorations.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class InputDecorations{
+
+  static InputDecoration authInputDecoration({
+    
+    required String hintText,
+    required String labelText,
+    IconData? prefixIcon
+
+  }){
+    return InputDecoration(
+                enabledBorder: const UnderlineInputBorder(
+                 borderSide: BorderSide(
+                  color: Color.fromARGB(255, 201, 90, 182),
+                   ),
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 155, 63, 127),
+                    width: 2,
+                  )
+                ),
+                hintText: hintText,
+                labelText: labelText,
+                labelStyle:  const TextStyle(
+                  color: Colors.grey,
+                ),
+                prefixIcon: prefixIcon != null
+                  ? Icon( prefixIcon,
+                  color: const Color.fromARGB(255, 155, 63, 127) )
+                  : null
+                );
+
+  }
+
+}
